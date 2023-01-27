d93ecd5e675b25174bc081e9fdd7b5768d010b5e
hadrianhu888 <hadrian.hu@gmail.com>
2023-01-27 17:02:28 -0500

    first commit

---
+++ Java_Test_0/src/helloWorld/helloWorld.java
@@ -0,0 +1,10 @@
+package helloWorld;
+
+public class helloWorld {
+// print out hello world on the screen.
+public static void main(String[] args) {
+        System.out.println("Hello World!");
+        // exit the program 
+        System.exit(0);
+    }
+}
